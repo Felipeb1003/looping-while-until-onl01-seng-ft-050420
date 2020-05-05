37aa4e111932c39b0b51046a499eae6d81f16951
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-05 23:42:06 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,4 +1,5 @@
 #It's our first year at Hogwarts and we're struggling to master the levitation charm, "Wingardium Leviosa". Currently, we have a levitation force of `6`. We need to have a levitation force of `10` in order to actually levitate that feather.
+#First, we'll write a while loop that will continue to `puts` the phrase "Wingardium Leviosa" while our levitation force is less than `10`. Every time we `puts` that phrase, we should increment our levitation force by `1`.
 #
 def using_until
   levitation_force = 6
